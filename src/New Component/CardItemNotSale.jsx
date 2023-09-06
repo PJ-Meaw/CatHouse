e9c00c723ab22dab5308d57cadaeaf3eca9fd701
PJ-Meaw <pathinya19@gmail.com>
2023-09-06 14:05:42 +0700

    Assign only 1 header

--- src/New Component/CardItemNotSale.jsx
+++ src/New Component/CardItemNotSale.jsx
@@ -7,17 +7,17 @@ import { Box,Flex, Text, Image } from '@chakra-ui/react';
 const CardItemNotSale = () => {
     const [users, setUsers] = useState([])
     const fetchUserData = () => {
-        fetch("http://localhost:3100/")
-          .then(response => {
-            return response.json()
-          })
-          .then(data => {
-            console.log(data)
-          })
-          .catch(error => {
-            // จัดการเมื่อเกิดข้อผิดพลาดในการเรียกใช้ API
-            console.error('Error fetching data:', error);
-          });
+        // fetch("http://localhost:3100/")
+        //   .then(response => {
+        //     return response.json()
+        //   })
+        //   .then(data => {
+        //     console.log(data)
+        //   })
+        //   .catch(error => {
+        //     // จัดการเมื่อเกิดข้อผิดพลาดในการเรียกใช้ API
+        //     console.error('Error fetching data:', error);
+        //   });
       }
       useEffect(()=>{
         fetchUserData()
